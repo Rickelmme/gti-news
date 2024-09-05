f9d799065237a0b2839099ab5aedc27aca89bc4c
Rickelmme <rickelmmecruz@gmail.com>
2024-09-05 10:37:51 -0300

    criacao do projeto, tailwind e tabelas

---
+++ resources/views/tela-teste.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite('resources/css/app.css')
+    <title>Tela de teste</title>
+</head>
+<body>
+    <h1 class="bg-black p-4 rounded-lg text-white">Hello World!</h1>
+</body>
+</html>
